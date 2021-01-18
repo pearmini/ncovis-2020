c3d56edcaf9d02ccdd32593bb8569a8d69e8ae1c
pearmini <subairui@icloud.com>
2021-01-18 14:07:09 +0800

    Updates

---
+++ precache-manifest.ba983f12317c8c0cccd0cdc6112d4513.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38331f9ace7b811495698f85d20b5a53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01d3f615df6c60a35743",
+    "url": "./static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "520ec8fca9104f8a541e",
+    "url": "./static/css/main.93ebdebe.chunk.css"
+  },
+  {
+    "revision": "01d3f615df6c60a35743",
+    "url": "./static/js/2.adf571e3.chunk.js"
+  },
+  {
+    "revision": "0706732b4e3da0d58aba5b97cac7e059",
+    "url": "./static/js/2.adf571e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520ec8fca9104f8a541e",
+    "url": "./static/js/main.b289ba00.chunk.js"
+  },
+  {
+    "revision": "f90c129fa4c0c04e3a7e",
+    "url": "./static/js/runtime-main.2808dafa.js"
+  },
+  {
+    "revision": "d2d2ad01025521a801d163e5e2c2673b",
+    "url": "./static/media/1.d2d2ad01.png"
+  },
+  {
+    "revision": "39c1bd14c318682bab82e76c86d175b3",
+    "url": "./static/media/2.39c1bd14.png"
+  },
+  {
+    "revision": "98b331a421d8ae0b16a8ca4d3db0543d",
+    "url": "./static/media/3.98b331a4.png"
+  },
+  {
+    "revision": "36d3cb06bb16c54c9fb7181d9de6e8b0",
+    "url": "./static/media/4.36d3cb06.png"
+  },
+  {
+    "revision": "849ab46f1dd039994517cce88ba86dc1",
+    "url": "./static/media/5.849ab46f.png"
+  },
+  {
+    "revision": "4e131bab583288ac529bff2aa9f37a77",
+    "url": "./static/media/6.4e131bab.png"
+  },
+  {
+    "revision": "6d888cb390df10a79124112ef803d4f0",
+    "url": "./static/media/china.6d888cb3.png"
+  },
+  {
+    "revision": "a9e6724183d6f66bd87a162f6c72eeca",
+    "url": "./static/media/hots.a9e67241.jpg"
+  },
+  {
+    "revision": "202e2a5d216e55e18bd1eb04d9eb2df8",
+    "url": "./static/media/logo.202e2a5d.woff2"
+  },
+  {
+    "revision": "3d4cbdbb8416d463f995b7c16e5ddd43",
+    "url": "./static/media/logo2.3d4cbdbb.woff2"
+  },
+  {
+    "revision": "0f6ed5a1490c3c985abb19c2ca6c661f",
+    "url": "./static/media/news.0f6ed5a1.jpg"
+  },
+  {
+    "revision": "1ad6cd23b87851fbe333b3b9487ccd0f",
+    "url": "./static/media/talk.1ad6cd23.jpg"
+  },
+  {
+    "revision": "9e85bbdff890b864c5e7b385ed8b6e56",
+    "url": "./static/media/zhihu.9e85bbdf.png"
+  }
+]);
